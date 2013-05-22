c1022659c3aad4a511e58022fd88897527f6906f
ojcar <vega@blogactivo.com>
2013-05-22 14:54:13 -0700

    more precompiled items

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Nacho::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( cosmo.css )
+  config.assets.precompile += %w( cosmo.css cosmo.js waypoints.min.js jquery.jknavigable.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
